eb4e4905f0ae7d1bc03a86d7b04689f14b344c27
arcade <arcadechan@gmail.com>
2022-12-27 02:46:48 -0800

    Updating footer and main background color

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,24 +5,36 @@ interface footerPropsI {
     socialPlatforms: any[]|null
 }
 
+const getSocialIcon = (platformName: string|null): JSX.Element|string => {
+    switch(platformName?.toLowerCase()){
+        case 'spotify':
+            return ( <img src='/icons/spotify.png' alt=''/> )
+        default:
+            return platformName || ''
+    }
+}
+
 const Footer = ({ socialPlatforms } : footerPropsI): JSX.Element => {
     const originYear: number = 2022
     const currentYear: number = new Date().getFullYear()
     const copyRightYear: string = originYear !== currentYear ? `${originYear} - ${currentYear}` : `${originYear}`
 
     return (
-        <footer id={styles.footer} className='max-w-screen-2xl m-auto mx-2 py-10 flex flex-col items-center'>
+        <footer id={styles.footer} className='max-w-screen-2xl mx-2 2xl:mx-auto py-10 flex flex-col items-center'>
             <img id={styles.logo} src='/logo/logo-black.png' className='mb-2 md:mb-0 my-5'/>
             <p className='my-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             {socialPlatforms && (
-                <div id={styles.socialBar}>
+                <div id={styles.socialBar} className='my-5 py-1.5 rounded-3xl drop-shadow-sm'>
                     {socialPlatforms.map(platform => (
                         <Link href={platform.url || ''} key={platform.name} passHref>
-                            <a>{platform.name || ''}</a>
+                            <a className='m-3' aria-label={platform.url}>
+                                {getSocialIcon(platform.name)}
+                            </a>
                         </Link>
                     ))}                    
                 </div>
             )}
+            <small className='my-5'>&copy; {currentYear} Amends. All Rights Reserved</small>
         </footer>
     )
 }
