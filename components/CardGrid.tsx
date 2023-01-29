ab861556e10031be446677bef390a95ee9e18a5b
arcade <arcadechan@gmail.com>
2023-01-28 18:38:09 -0800

    Committing latest progress on attempt to integrate Plaiceholder

--- components/CardGrid.tsx
+++ components/CardGrid.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
-import { Template } from 'tinacms'
+import React, { useState, useEffect } from 'react'
 import styles from '../styles/components/CardGrid.module.scss'
+import { getPlaiceholder } from 'plaiceholder'
 
 interface CardGridProps {
   columnCount: string
@@ -31,6 +31,11 @@ interface ManualCard {
   ctaText: string
 }
 
+interface CardImageProps {
+  cardImage: string | undefined
+  columnCount: string | undefined
+}
+
 const getCardUrl = (card: Card): string =>
 {
   if(card?.manualCard?.url) return card.manualCard.url
@@ -53,9 +58,30 @@ const getAriaLabel = (card: Card): string =>
   return ''
 }
 
-const CardImage = ({cardImage}: {cardImage: string | undefined }): JSX.Element => 
+const CardImage = ({cardImage, columnCount}: CardImageProps): JSX.Element => 
 {
   if(cardImage) {
+    let sizes = ''
+
+    switch(columnCount)
+    {
+      case 'one':
+        sizes = '100vw'
+        break;
+      case 'two':
+        sizes = '100vw, (min-width: 768px) 50vw'
+        break;
+      case 'three':
+        sizes = '100vw, (min-width: 768px) 50vw, (min-width: 1024px) 33vw'
+        break;
+      case 'four':
+        sizes = '100vw, (min-width: 768px) 50vw, (min-width: 1024px) 25vw'
+        break;
+      default:
+        sizes = '100vw, (min-width: 768px) 50vw'
+        break;
+    }
+
     return (
       <div className={styles.cardGridImageContainer}>
         <Image
@@ -63,8 +89,9 @@ const CardImage = ({cardImage}: {cardImage: string | undefined }): JSX.Element =
           alt=''
           layout='fill'
           objectFit='cover'
-          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+          sizes={sizes}
           className={styles.cardGridImage}
+          placeholder='empty'
         />
       </div>
     )
@@ -77,54 +104,86 @@ const CardGrid = ({ componentProps }: { componentProps:  CardGridProps}): JSX.El
 {
   const { columnCount = 'one', sectionTitle, cards } = componentProps
 
-  return (
-    <section className={`${styles.cardGrid} px-12 py-4 max-w-screen-2xl 2xl:mx-auto`}>
-      {sectionTitle && <h2 className={styles.cardGridTitle}>{sectionTitle}</h2>}
-      <div className={`${styles.cardGridCardContainer} ${styles[`${columnCount}ColumnGrid`]}`}>
-        {cards && cards?.length > 0 && cards.map((card, i: number) => {
-          const cardImage = card?.manualCard?.image || card?.referenceCard?.heroImage
-          const showCtaButton = card?.manualCard?.showCtaButton
+  const [ imagePlaiceholders, setImagePlaiceholders ] = useState([{}])
 
-          const cardContent = (
-            <>
-              <CardImage cardImage={cardImage}/>
-              <div className={styles.cardTextLabels}>
-                {(card?.referenceCard?.title || card?.manualCard?.title) && (
-                  <h2 className={showCtaButton ? styles.cardTitleCentered : styles.cardTitle} aria-hidden>
-                    {card?.referenceCard?.title || card?.manualCard?.title}
-                  </h2>
-                )}
-                {(card?.referenceCard?.subtitle || card?.manualCard?.title) && (
-                  <h3 className={styles.cardSubtitle} aria-hidden>
-                    {card?.referenceCard?.subtitle || card?.manualCard?.subtitle}
-                  </h3>
-                )}
-                {showCtaButton && (
-                  <Link href={getCardUrl(card)} passHref>
-                    <a aria-label={getAriaLabel(card)} className={styles.cardCtaButton}>
-                      {card?.manualCard?.ctaText || ''}
-                    </a>
-                  </Link>
-                )}
-              </div>
-            </>
-          )
+  useEffect(() => {
+    const plaiceholders: any[] = []
+    const getImagePlaiceholders = async (): Promise<void> =>
+    {
+      if(cards?.length)
+      {
+        for(let i = 0; i < cards.length; i++)
+        {
+          const card = cards[i]
+          const cardImage = card?.referenceCard?.heroImage || card?.manualCard?.image
+          if(cardImage)
+          {
+            const base64 = await getPlaiceholder(cardImage).then(({ base64 }) => base64);
+            console.log({base64});
+            if(base64)
+            {
+              plaiceholders.push({
+                src: cardImage,
+                base64
+              })
+            }
+          }
+        }
 
-          return (
-            <article className={`${styles.card} ${cardImage ? styles.cardHasImage : styles.cardHasNoImage }`} key={i}>
-              <Link href={getCardUrl(card)} passHref>
-                {card?.manualCard?.showCtaButton ? cardContent : (
-                  <a aria-label={getAriaLabel(card)} className={styles.cardAnchor}>
-                    {cardContent}
+        setImagePlaiceholders(plaiceholders)
+      }
+    }
+
+    // getImagePlaiceholders().then(data =>
+    // {
+    //   console.log({data})
+    // })
+  }, [])
+  
+
+  if(imagePlaiceholders?.length)
+  {
+    return (
+      <section className={`${styles.cardGrid} px-12 py-4 max-w-screen-2xl 2xl:mx-auto`}>
+        {sectionTitle && <h2 className={styles.cardGridTitle}>{sectionTitle}</h2>}
+        <div className={`${styles.cardGridCardContainer} ${styles[`${columnCount}ColumnGrid`]}`}>
+          {cards && cards?.length > 0 && cards.map((card, i: number) => {
+            const cardImage = card?.manualCard?.image || card?.referenceCard?.heroImage
+            const cardTitle = card?.referenceCard?.title || card?.manualCard?.title
+            const cardSubtitle = card?.referenceCard?.subtitle || card?.manualCard?.subtitle
+            const showCtaButton = card?.manualCard?.showCtaButton
+
+            return (
+              <article className={`${styles.card} ${cardImage ? styles.cardHasImage : styles.cardHasNoImage }`} key={i}>
+                <Link href={getCardUrl(card)} passHref>
+                  <a aria-label={getAriaLabel(card)} className={`${styles.cardAnchor} ${showCtaButton ? styles.cardAnchorWithCta : ''}`}>
+                    <CardImage cardImage={cardImage} columnCount={columnCount}/>
+                    <div className={styles.cardTextLabels}>
+                      {cardTitle && (
+                        <h2 className={showCtaButton ? styles.cardTitleCentered : styles.cardTitle} aria-hidden>{cardTitle}</h2>
+                      )}
+                      {cardSubtitle && (
+                        <h3 className={styles.cardSubtitle} aria-hidden>{cardSubtitle}</h3>
+                      )}
+                      {showCtaButton && (
+                        <span className={styles.cardCtaButton}>
+                          {card?.manualCard?.ctaText || ''}
+                        </span>
+                      )}
+                    </div>
                   </a>
-                )}
-              </Link>
-            </article>
-          )
-        })}
-      </div>
-    </section>
-  )
+                </Link>
+              </article>
+            )
+          })}
+        </div>
+      </section>
+    )
+  }
+  else
+  {
+    return <></>
+  }
 }
 
 export default CardGrid
