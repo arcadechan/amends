ffe58673bf5818148958315d7e2c16f6aa4d1cf1
unknown <arcadechan@gmail.com>
2023-02-01 23:07:49 -0800

    Updating font declrations for @next/font - Removed Google Inter fonts - Removed fonts css file - Added @next/font for all font references - Updated tailwind config to reference new font vars

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,31 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-library.add(far, fas, fab)
-config.autoAddCss = false
+import { Inter } from '@next/font/google'
+import localFont from '@next/font/local'
 import '../styles/main.css'
 
+library.add(far, fas, fab)
+config.autoAddCss = false
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter'
+})
+
+const candyBean = localFont({
+  src: '../public/fonts/candy-beans.otf',
+  variable: '--font-candy'
+})
+
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className={`${inter.variable} ${candyBean.className}`}>
+        <Component {...pageProps}/>
+      </div>
+    </>  
+  )
 }
 
 export default App
