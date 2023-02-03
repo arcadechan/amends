2cfb6917a3384cd0f7eed81da4d3e8798ca21a91
arcade <arcadechan@gmail.com>
2023-02-02 21:35:23 -0800

    Committing progress of error pages.

---
+++ pages/500.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { Head, Layout } from "../components/layout";
+import { client } from '../.tina/__generated__/client'
+import styles from '../styles/pages/Error.module.scss'
+
+export const getStaticProps = async (ctx: any) =>
+{
+  const siteMetaQuery = await client.queries.meta({ relativePath: 'meta.mdx' })
+  const siteMeta = siteMetaQuery?.data?.meta || null
+  
+  return {
+    props: {
+      siteMeta: siteMeta
+    }
+  }
+}
+
+const Custom500 = (props: any): JSX.Element =>
+{
+  const { siteMeta } = props
+
+  return (
+    <Layout siteMeta={siteMeta} className={styles.error}>
+      <Head>
+        <title>500 | Amends</title>
+      </Head>
+      <section>
+        <h1>Oh no! Something went wrong 😓.</h1>
+        <Link href='/'>Back to the homepage.</Link>
+      </section>
+    </Layout>
+  )
+}
+
+export default Custom500
