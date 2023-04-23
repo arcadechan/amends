d93c0bd355cdc06f294b7989b9cd6213b69c96f5
arcade <arcadechan@gmail.com>
2023-04-22 21:07:52 -0700

    Various updates - Created form Success page - Added Lottie library - Added Framer Motion library

---
+++ app/success/Success.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { motion } from 'framer-motion'
+import Lottie from 'lottie-react'
+import Link from 'next/link'
+import successAnimation from '../../public/animations/success.json'
+
+export default function Success()
+{
+  const initial = { opacity: 0 }
+  const animate = { opacity: 1 }
+  const transition = { duration: 0.5, delay: 1.3 }
+
+  return (
+    <section
+      id='success'
+      className='text-center mx-auto my-10'
+    >
+      <motion.h1
+        className='text-5xl font-candy'
+        initial={initial}
+        animate={animate}
+        transition={transition}
+      >
+        Success
+      </motion.h1>
+      <Lottie
+        id='test-lottie'
+        className='w-72 h-72 mx-auto'
+        animationData={successAnimation} loop={false}
+      />
+      <motion.p
+        className='text-2xl font-inter font-bold'
+        initial={initial}
+        animate={animate}
+        transition={transition}
+      >
+        Thanks for your submission!
+      </motion.p>
+      <motion.div
+        initial={initial}
+        animate={animate}
+        transition={transition}
+      >
+        <Link
+          href='/'
+          className='inline-block bg-black text-yellow p-4 rounded-md'
+        >
+          To home
+        </Link>
+      </motion.div>
+    </section>
+  )
+}
---
+++ app/success/page.tsx
@@ -0,0 +1,11 @@
+import Success from "./Success"
+
+export const metadata = {
+  title: 'Success | Amends',
+  description: ''
+}
+
+export default function Page()
+{
+  return <Success/>
+}
