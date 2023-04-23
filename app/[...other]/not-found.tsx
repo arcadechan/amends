16fe91cdd6f70c564d2afb29262cebdefdac3f8d
arcade <arcadechan@gmail.com>
2023-04-23 11:47:22 -0700

    Updating 404 page - Added Lottie animation - Migrated styles to inline Tailwind CSS classes for utility-first approach - Tweaked styles

--- app/[...other]/not-found.tsx
+++ app/[...other]/not-found.tsx
@@ -1,19 +1,33 @@
 'use client'
 
 import Link from "next/link";
-import styles from '../../styles/pages/Error.module.scss'
+import Lottie from 'lottie-react'
+import compassAnimation from '../../public/animations/404-compass.json'
 import '../../styles/main.css'
 
 export default function NotFound(): JSX.Element
 {
   return (
-    <section className={styles.error}>
-      <h1 className={styles.errorH1}>
-        <span className={styles.errorCode}>404</span><br/>
-        <span className={styles.errorMessage}>PAGE NOT FOUND.</span>  
-      </h1>
-      <h2 className={styles.text}>This page doesn&apos;t exist.</h2><br/>
-      <Link href='/' className={styles.linkToHome}>Back to the homepage.</Link>
+    <section className='mx-auto text-center max-w-screen-2xl py-20 px-12'>
+      <div className='font-candy'>
+        <h1 className='text-8xl'>404</h1>
+        <h2 className='text-3xl'>PAGE NOT FOUND.</h2>
+      </div>
+      <Lottie
+        className='w-72 h-72 mx-auto'
+        animationData={compassAnimation}
+        
+      />
+      <h2 className='text-xl mb-5'>
+        Uh oh. There&apos;s nothing here.<br/>
+        Was there supposed to be?
+      </h2>
+      <Link
+        href='/'
+        className='inline-block bg-black text-yellow py-3 px-6 rounded-full font-inter text-base'
+      >
+        Back to the homepage.
+      </Link>
     </section>
   )
 }
