2874a13ec62ce9777ca2a1c1b0ba04436d345fa3
arcade <arcadechan@gmail.com>
2023-12-13 12:54:27 -0800

    Finalizing domain check on theme cookie

--- src/actions/ThemeSwitch.ts
+++ src/actions/ThemeSwitch.ts
@@ -5,12 +5,6 @@ import { cookies } from 'next/headers'
 export default async function handleThemeSwitch(formData: FormData) {
   const selectedTheme = formData.get('theme') as string
 
-  console.log({
-    DOMAIN: process.env.DOMAIN,
-    VERCEL_URL: process.env.VERCEL_URL,
-    VERCEL_BRANCH_URL: process.env.VERCEL_BRANCH_URL
-  })
-
   if (selectedTheme) {
     cookies().set({
       name: 'theme',
@@ -20,11 +14,7 @@ export default async function handleThemeSwitch(formData: FormData) {
       httpOnly: false,
       sameSite: 'strict',
       secure: false,
-      domain:
-        process.env.DOMAIN ||
-        process.env.VERCEL_URL ||
-        process.env.VERCEL_BRANCH_URL ||
-        'localhost'
+      domain: process.env.DOMAIN || process.env.VERCEL_URL || 'localhost'
     })
   }
 }
