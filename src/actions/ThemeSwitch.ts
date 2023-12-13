269a381881fe07b95c23ba152fb22bef6fae74b2
arcade <arcadechan@gmail.com>
2023-12-13 12:46:45 -0800

    Adding logging to debug domain

--- src/actions/ThemeSwitch.ts
+++ src/actions/ThemeSwitch.ts
@@ -5,6 +5,12 @@ import { cookies } from 'next/headers'
 export default async function handleThemeSwitch(formData: FormData) {
   const selectedTheme = formData.get('theme') as string
 
+  console.log({
+    DOMAIN: process.env.DOMAIN,
+    VERCEL_URL: process.env.VERCEL_URL,
+    VERCEL_BRANCH_URL: process.env.VERCEL_BRANCH_URL
+  })
+
   if (selectedTheme) {
     cookies().set({
       name: 'theme',
