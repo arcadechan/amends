b9ea36a06bed9fb1c60450977b7e84aa1fd657e2
arcade <arcadechan@gmail.com>
2026-05-08 23:11:38 -0700

    Removing duplicate SCSS import.

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,5 @@
 import { vitePreprocess } from '@astrojs/svelte';
-import path from 'node:path';
 
 export default {
-	preprocess: vitePreprocess({
-		style: {
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: `@use "${path.resolve('src/styles/index')}" as *;`
-					}
-				}
-			}
-		}
-	}),
+  preprocess: vitePreprocess(),
 }
